e2573ad00c8aac20cd0cc5acc2525595e33f101f
Timothy Chen <tnachen@gmail.com>
2017-01-15 23:30:44 +0000

    Add benchmark agent client calls

--- clients.go
+++ clients.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"path"
 
 	"github.com/go-resty/resty"
+	"github.com/hyperpilotio/container-benchmarks/benchmark-agent/apis"
 	"github.com/spf13/viper"
 )
+
+type ApiResponse struct {
+	Data  string `json:"data"`
+	Error bool   `json:"error"`
+}
 
 type DeployerClient struct {
 	Url string
@@ -41,4 +49,50 @@ func NewBenchmarkAgentClient(config *viper.Viper) *BenchmarkAgentClient {
 	return &BenchmarkAgentClient{
 		Url: config.GetString("benchmarkAgentUrl"),
 	}
+}
+
+func (client *BenchmarkAgentClient) CreateBenchmark(benchmark *apis.Benchmark) error {
+	response, err := resty.R().
+		Post(path.Join(client.Url, "benchmarks"))
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode() != 202 {
+		apiResponse := ApiResponse{}
+		if err := json.Unmarshal(response.Body(), &apiResponse); err != nil {
+			return errors.New("Unable to parse failed api response: " + err.Error())
+		} else {
+			return errors.New(apiResponse.Data)
+		}
+	}
+
+	return nil
+}
+
+func (client *BenchmarkAgentClient) UpdateBenchmarkResources(benchmarkName string, resources *apis.Resources) error {
+	resourcesJson, marshalErr := json.Marshal(resources)
+	if marshalErr != nil {
+		return errors.New("Unable to marshal resources to JSON: " + marshalErr.Error())
+	}
+
+	response, err := resty.R().
+		SetBody(string(resourcesJson)).
+		Put(path.Join(client.Url, "benchmarks", benchmarkName, "resources"))
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode() != 202 {
+		apiResponse := ApiResponse{}
+		if err := json.Unmarshal(response.Body(), &apiResponse); err != nil {
+			return errors.New("Unable to parse failed api response: " + err.Error())
+		} else {
+			return errors.New(apiResponse.Data)
+		}
+	}
+
+	return nil
 }
