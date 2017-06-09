7bee0c0524db48aa238211583984a5a487e2f671
alan.yang <jpra1113@gmail.com>
2017-06-09 17:33:10 +0800

    Add ServiceNames

--- models.go
+++ models.go
@@ -26,10 +26,11 @@ type SLO struct {
 }
 
 type ApplicationConfig struct {
-	Name       string     `bson:"name" json:"name"`
-	LoadTester LoadTester `bson:"loadTester" json:"loadTester"`
-	Type       string     `bson:"type" json:"type"`
-	SLO        SLO        `bson:"slo" json:"slo"`
+	Name         string     `bson:"name" json:"name"`
+	ServiceNames []string   `bson:"serviceNames" json:"serviceNames"`
+	LoadTester   LoadTester `bson:"loadTester" json:"loadTester"`
+	Type         string     `bson:"type" json:"type"`
+	SLO          SLO        `bson:"slo" json:"slo"`
 }
 
 type IntensityArgument struct {
